59baa32a22783967b461bc2b5f9ae95c09beb4b6
Carl <shikang.liu@mail.mcgill.ca>
2020-01-18 11:58:23 -0800

    save style config

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,5 @@
 export { Hierarchy } from './Hierarchy/Hierarchy'
 export { Button } from './Button/Button'
 export { SlideShow } from './SlideShow/SlideShow'
+
+import "./SlideShow/SlideShow.scss";
