4e62ce8b3e3a1b9cacf663333d87e803454ab7e2
Carl <shikang.liu@mail.mcgill.ca>
2020-02-02 23:54:40 +0800

    update BarChart

--- src/components/BarChart/BarChart.stories.tsx
+++ src/components/BarChart/BarChart.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
 import { storiesOf } from '@storybook/react'
-import { BarChart } from './BarChart'
+import BarChart from './index'
 
 storiesOf('BarChart', module).add('BarChart', () => <BarChart/>)
--- src/components/BarChart/BarChart.tsx
+++ src/components/BarChart/BarChart.tsx
@@ -1,74 +1,108 @@
 import * as React from "react";
-const { useRef, useEffect, useState } = React;
 import { select, axisBottom, axisRight, scaleLinear, scaleBand } from "d3";
+import ResizeObserver from "resize-observer-polyfill";
 
-export const BarChart = () => {
-    const [data, setData] = useState([25, 30, 45, 60, 10, 65, 75]);
+const { useRef, useEffect, useState } = React;
+
+interface IBarChartProps {
+    data: number[];
+}
+const useResizeObserver = ref => {
+    const [dimensions, setDimensions] = useState(null as any);
+    useEffect(() => {
+        const observeTarget = ref.current;
+        const resizeObserver = new ResizeObserver(entries => {
+            entries.forEach(entry => {
+                setDimensions(entry.contentRect as any);
+            });
+        });
+        resizeObserver.observe(observeTarget);
+        return () => {
+            resizeObserver.unobserve(observeTarget);
+        };
+    }, [ref]);
+    return dimensions;
+};
+
+
+const BarChart = ({ data }: IBarChartProps) => {
     const svgRef = useRef(null);
+    const wrapperRef = useRef(null);
+    const dimensions = useResizeObserver(wrapperRef);
 
     // will be called initially and on every data change
     useEffect(() => {
         const svg = select(svgRef.current);
+        console.log(dimensions);
+
+        if (!dimensions) return;
+
+        // scales
         const xScale = scaleBand()
             .domain(data.map((value, index) => index))
-            .range([0, 300])
+            .range([0, dimensions.width]) // change
             .padding(0.5);
 
         const yScale = scaleLinear()
-            .domain([0, 150])
-            .range([150, 0]);
+            .domain([0, 150]) // todo
+            .range([dimensions.height, 0]); // change
 
         const colorScale = scaleLinear()
             .domain([75, 100, 150])
             .range(["green", "orange", "red"])
             .clamp(true);
 
+        // create x-axis
         const xAxis = axisBottom(xScale).ticks(data.length);
-
         svg
             .select(".x-axis")
-            .style("transform", "translateY(150px)")
+            .style("transform", `translateY(${dimensions.height}px)`)
             .call(xAxis);
 
+        // create y-axis
         const yAxis = axisRight(yScale);
         svg
             .select(".y-axis")
-            .style("transform", "translateX(300px)")
+            .style("transform", `translateX(${dimensions.width}px)`)
             .call(yAxis);
 
+        // draw the bars
         svg
             .selectAll(".bar")
             .data(data)
-            .join(
-                enter => enter.append("rect")
-                    .attr("fill", "green")
-                    .text(d => d),
-                update => update
-                    .attr("fill", "gray")
-            )
+            .join("rect")
             .attr("class", "bar")
-
             .style("transform", "scale(1, -1)")
             .attr("x", (value, index) => xScale(index))
-            .attr("y", -150)
+            .attr("y", -dimensions.height)
             .attr("width", xScale.bandwidth())
+            .on("mouseenter", (value, index) => {
+                svg
+                    .selectAll(".tooltip")
+                    .data([value])
+                    .join(enter => enter.append("text").attr("y", yScale(value) - 4))
+                    .attr("class", "tooltip")
+                    .text(value)
+                    .attr("x", xScale(index) + xScale.bandwidth() / 2)
+                    .attr("text-anchor", "middle")
+                    .transition()
+                    .attr("y", yScale(value) - 8)
+                    .attr("opacity", 1);
+            })
+            .on("mouseleave", () => svg.select(".tooltip").remove())
             .transition()
             .attr("fill", colorScale)
-            .attr("height", value => 150 - yScale(value));
-    }, [data]);
+            .attr("height", value => dimensions.height - yScale(value));
+    }, [data, dimensions]);
 
     return (
-        <>
-            <svg ref={svgRef} width={500} height={500}>
+        <div ref={wrapperRef} style={{ marginBottom: "2rem" }}>
+            <svg ref={svgRef}>
                 <g className="x-axis" />
                 <g className="y-axis" />
             </svg>
-            <button onClick={() => setData(data.map(value => value + 5))}>
-                Update data
-            </button>
-            <button onClick={() => setData([25, 30, 45, 60, 10, 65, 75])}>
-                Reset
-            </button>
-        </>
+        </div>
     );
 }
+
+export default BarChart;
