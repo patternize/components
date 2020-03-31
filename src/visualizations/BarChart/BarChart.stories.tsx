976a800c89493ccb553cc05a04ecc8a736fb1fef
Carl <shikang.liu@mail.mcgill.ca>
2020-03-30 22:12:52 -0700

    Add horizontal bar-chart

--- src/visualizations/BarChart/BarChart.stories.tsx
+++ src/visualizations/BarChart/BarChart.stories.tsx
@@ -2,15 +2,39 @@ import * as React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { VerticalBarChart } from './VerticalBarChart';
+import { HorizontalBarChart } from './HorizontalBarChart';
 import { Button } from 'components/Button';
 
-storiesOf('D3 Modules', module).add('BarChart', () =>
+storiesOf('D3 Modules', module).add('VerticalBarChart', () =>
   React.createElement(() => {
     const [data, setData] = React.useState([25, 30, 45, 60, 10, 65, 75]);
 
     return (
       <div className={'controller'}>
         <VerticalBarChart data={data} />
+        <Button onClick={() => setData(data.map(value => value + 5))}>
+          Update data
+        </Button>
+        <Button onClick={() => setData(data.filter(value => value < 35))}>
+          Filter data
+        </Button>
+        <Button
+          onClick={() => setData([...data, Math.round(Math.random() * 100)])}
+        >
+          Add data
+        </Button>
+      </div>
+    );
+  })
+);
+
+storiesOf('D3 Modules', module).add('HorizontalBarChart', () =>
+  React.createElement(() => {
+    const [data, setData] = React.useState([25, 30, 45, 60, 10, 65, 75]);
+
+    return (
+      <div className={'controller'}>
+        <HorizontalBarChart data={data} />
         <Button onClick={() => setData(data.map(value => value + 5))}>
           Update data
         </Button>
---
+++ src/visualizations/BarChart/HorizontalBarChart.tsx
@@ -0,0 +1,97 @@
+import * as React from 'react';
+import * as d3 from 'd3';
+import { useResizeObserver } from 'hooks';
+import './BarChart.scss';
+
+const { useRef, useEffect } = React;
+interface IBarChartProps {
+  data: number[];
+}
+
+export const HorizontalBarChart = ({ data }: IBarChartProps) => {
+  const svgRef = useRef(null);
+  const wrapperRef = useRef(null);
+  const dimensions = useResizeObserver(wrapperRef);
+
+  // will be called initially and on every data change
+  useEffect(() => {
+    const svg = d3.select(svgRef.current);
+
+    if (!dimensions) return;
+    const margin = { top: 80, right: 40, bottom: 40, left: 80 };
+    const width = dimensions.width - margin.left - margin.right;
+    const height = dimensions.height - margin.left - margin.right;
+    // scales
+    const xMax = d3.max(data);
+    const xScale = d3
+      .scaleLinear()
+      .domain([0, xMax]) // scale from 0 to xMax
+      .range([0, width]); // map to 0 to width pixels
+
+    const yScale = d3
+      .scaleBand()
+      .domain(data.map((d, i) => i))
+      .range([0, height])
+      .paddingInner(0.25);
+
+    const colorScale = d3
+      .scaleLinear()
+      .domain([75, 100, 150])
+      .range(['green', 'orange', 'red'])
+      .clamp(true);
+
+    // append g at svgRef.current
+    svg
+      .append('g')
+      .attr('transform', `translate(${margin.left}, ${margin.top})`);
+
+    // create and draw x-axis
+    const xAxis = d3.axisTop(xScale).ticks(0);
+    svg.select('.x-axis').call(xAxis);
+
+    // create and draw y-axis
+    const yAxis = d3.axisLeft(yScale).tickSize(data.length);
+    svg.select('.y-axis').call(yAxis);
+
+    // draw bars
+    const bars = svg
+      .selectAll('.bar')
+      .data(data)
+      .join('rect')
+      .attr('class', 'bar')
+      .attr('y', (d, idx) => yScale(idx))
+      .attr('height', yScale.bandwidth());
+
+    // add tooltips on mouseenter event
+    bars.on('mouseenter', (val, idx) => {
+      svg
+        .selectAll('.tooltip')
+        .data([val])
+        .join(enter => enter.append('text').attr('x', xScale(val) + 4))
+        .attr('class', 'tooltip')
+        .text(val)
+        .attr('y', yScale(idx) + yScale.bandwidth() / 1.75) // @todo: this is not perfectly in the middle
+        .transition()
+        .attr('x', xScale(val) + 10)
+        .attr('opacity', 1);
+    });
+
+    // remove tooltips on mouseleave event
+    bars.on('mouseleave', () => svg.select('.tooltip').remove());
+
+    // add transitions
+    bars
+      .transition()
+      .attr('fill', colorScale)
+      .attr('width', d => xScale(d));
+  }, [data, dimensions]);
+
+  return (
+    <div ref={wrapperRef}>
+      <svg ref={svgRef} className={'bar-chart'} style={{ height: '500px' }}>
+        <g className='x-axis' />
+        <g className='y-axis' />
+      </svg>
+    </div>
+  );
+};
