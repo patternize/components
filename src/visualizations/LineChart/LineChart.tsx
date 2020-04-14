8c658c69e4d013539506bd8ceb4bd7bf01e403d7
Carl <shikang.liu@mail.mcgill.ca>
2020-04-13 23:32:14 -0700

    Add LineChart component

--- src/visualizations/LineChart/LineChart.tsx
+++ src/visualizations/LineChart/LineChart.tsx
@@ -1,27 +1,41 @@
 import * as React from 'react';
 import * as d3 from 'd3';
 import { useResizeObserver } from 'hooks';
-import './BarChart.scss';
 
 const { useRef, useEffect } = React;
 
 type TimeSeriesDataPoints = {
-  date: Date,
+  date: Date | string; // in format of utc or user defined format that is convertible with parseDate function
   value: number;
-}
+};
 type TimeSeries = {
-  name: string,
-  color: string,
-  values: TimeSeriesDataPoints[]
+  name: string;
+  color: string;
+  values: TimeSeriesDataPoints[];
 };
 
 interface ILineChartProps {
   series: TimeSeries[];
-  dates: Date[];
-  maxValue: number;
+  parseDate?: (string) => Date;
 }
 
-export const LinearChart = ({ series, dates, maxValue }: ILineChartProps) => {
+const _parseDate = string => d3.utcParse('%Y-%m-%d')(string);
+const _mergeDates = (series: TimeSeries[]): Date[] => {
+  let dates = [];
+  series.map(timeSeries => {
+    dates = [timeSeries.values.map(e => _parseDate(e.date)), ...dates];
+  });
+  return dates;
+};
+const _mergeValues = (series: TimeSeries[]): number[] => {
+  let values = [];
+  series.map(timeSeries => {
+    values = [timeSeries.values.map(e => e.value), ...values];
+  });
+  return values;
+};
+
+export const LinearChart = ({ series, parseDate }: ILineChartProps) => {
   const svgRef = useRef(null);
   const wrapperRef = useRef(null);
   const dimensions = useResizeObserver(wrapperRef);
@@ -38,18 +52,18 @@ export const LinearChart = ({ series, dates, maxValue }: ILineChartProps) => {
     // Scale data. Make sure to use ScaleTime()
     const xScale = d3
       .scaleTime()
-      .domain(d3.extent(dates))
+      .domain(d3.extent(_mergeDates(series)))
       .range([0, width]);
 
     const yScale = d3
       .scaleLinear()
-      .domain([0, maxValue])
+      .domain([0, d3.max(_mergeValues(series))])
       .range([height, 0]);
 
     // line generator
     const lineGen = d3
       .line()
-      .x(d => xScale(d.date))
+      .x(d => xScale(_parseDate(d.date)))
       .y(d => yScale(d.value));
 
     // append g at svgRef.current
@@ -59,7 +73,7 @@ export const LinearChart = ({ series, dates, maxValue }: ILineChartProps) => {
 
     // Draw x axis.
     const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
-    const xAxisDraw = svg
+    svg
       .append('g')
       .attr('transform', `translate(0, ${height})`)
       .attr('class', 'a axis')
@@ -72,7 +86,7 @@ export const LinearChart = ({ series, dates, maxValue }: ILineChartProps) => {
       .tickSizeOuter(0)
       .tickSizeInner(-width);
 
-    const yAxisDraw = svg
+    svg
       .append('g')
       .attr('class', 'y axis')
       .call(yAxis);
@@ -87,7 +101,7 @@ export const LinearChart = ({ series, dates, maxValue }: ILineChartProps) => {
       .attr('d', d => lineGen(d.values))
       .style('fill', 'none')
       .style('stroke', d => d.color);
-  }, [series, dates, maxValue, dimensions]);
+  }, [series, dimensions]);
 
   return (
     <div ref={wrapperRef}>
